c3da4c48e3f54fae04577c39bb894a37e08e4f89
ivandyuzhev <axensk@yandex.ru>
2024-03-25 17:33:36 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+
+    }
+
+    public void printInfo() {
+        System.out.println("Is there a mistake: " + active);
+        System.out.println("Error number: " + status);
+        System.out.println("Error message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error notError = new Error();
+        Error criticalError = new Error(true, 505, "I will sell the car");
+        Error notCriticalError = new Error(true, 100500, "Your computer burned down");
+        notError.printInfo();
+        criticalError.printInfo();
+        notCriticalError.printInfo();
+    }
+}
