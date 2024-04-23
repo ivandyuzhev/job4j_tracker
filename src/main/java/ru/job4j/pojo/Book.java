6e4c3c198f302acda91478714570f7dcc3cf6d60
ivandyuzhev <axensk@yandex.ru>
2024-04-23 11:31:12 +0700

    3. Массивы и модели. [#174892 #492567]

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -6,23 +6,6 @@ public class Book {
     private String name;
     private int pages;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book cleanCode = (Book) o;
-        return pages == cleanCode.pages && Objects.equals(name, cleanCode.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, pages);
-    }
-
     public Book(String name, int pages) {
         this.name = name;
         this.pages = pages;
@@ -42,5 +25,22 @@ public class Book {
 
     public void setPages(int pages) {
         this.pages = pages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book cleanCode = (Book) o;
+        return pages == cleanCode.pages && Objects.equals(name, cleanCode.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, pages);
     }
 }
