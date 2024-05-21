5c05d111ad5b1dd035c7f9cdfe9dca1795e99268
ivandyuzhev <axensk@yandex.ru>
2024-05-21 16:47:18 +0700

    3.3. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,14 +16,9 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
                 System.out.println("=== Вывод всех заявок ===");
                 Item[] items = tracker.findAll();
-                for (Item item : items) {
-                    System.out.println(item);
-                }
                 if (items.length > 0) {
                     for (Item item : items) {
                         System.out.println(item);
@@ -31,6 +26,17 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 6) {
+                run = false;
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+                System.out.println("Заявка изменена успешно.");
             }
         }
     }
@@ -52,4 +58,5 @@ public class StartUI {
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
     }
+
 }
