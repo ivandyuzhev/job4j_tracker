bac22403ec14fcf6f99c3290c691b27f0b4e8997
ivandyuzhev <axensk@yandex.ru>
2024-05-21 10:25:55 +0700

    10. toString [#310018 #497016]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -53,12 +53,4 @@ public class Item {
                 + '}';
     }
 
-    public static class StartUI {
-        public static void main(String[] args) {
-            Item item = new Item();
-            DateTimeFormatter date = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-            System.out.println(item.getCreated().format(date));
-        }
-
-    }
 }
