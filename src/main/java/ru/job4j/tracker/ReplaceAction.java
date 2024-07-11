9a928ddd9f431db1233103ffbfdbb3125e77f193
ivandyuzhev <axensk@yandex.ru>
2024-07-11 19:41:43 +0700

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class ReplaceAction implements UserAction {
+    private final Output output;
+
+    public ReplaceAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Изменение заявки";
@@ -8,15 +14,15 @@ public class ReplaceAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Изменение заявки ===");
+       output.println("=== Изменение заявки ===");
         int id = input.askInt("Введите id: ");
         String name = input.askStr("Введите имя: ");
         Item item = new Item(name);
         boolean isReplaced = tracker.replace(id, item);
         if (isReplaced) {
-            System.out.println("Заявка изменена успешно.");
+           output.println("Заявка изменена успешно.");
         } else {
-            System.out.println("Ошибка: Заявка с данным id не найдена.");
+           output.println("Ошибка: Заявка с данным id не найдена.");
         }
         return true;
     }
