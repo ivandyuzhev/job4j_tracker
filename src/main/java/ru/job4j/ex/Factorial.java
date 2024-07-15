ae28604932052d6ea8def6b2a70a0e57e533b5fb
ivandyuzhev <axensk@yandex.ru>
2024-07-15 12:47:28 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #504841]

--- src/main/java/ru/job4j/ex/Factorial.java
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -2,12 +2,12 @@ package ru.job4j.ex;
 
 public class Factorial {
     public int calc(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number could not be less than 0");
+        }
         int result = 1;
         for (int index = 2; index <= number; index++) {
             result *= index;
-        }
-        if (number < 0) {
-            throw new IllegalArgumentException("Number could not be less than 0");
         }
         return result;
     }
