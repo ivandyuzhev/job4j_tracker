734e99b44b49025f62d6238ded4ef8705baadcd7
ivandyuzhev <axensk@yandex.ru>
2024-07-16 15:10:58 +0700

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,13 @@
 package ru.job4j.tracker;
 
-import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.action.Create;
+import ru.job4j.tracker.action.Delete;
+import ru.job4j.tracker.action.Exit;
+import ru.job4j.tracker.action.FindAll;
+import ru.job4j.tracker.action.FindById;
+import ru.job4j.tracker.action.FindByName;
+import ru.job4j.tracker.action.Replace;
+import ru.job4j.tracker.action.UserAction;
 
 public class StartUI {
     private final Output output;
