0728d71170d1ec2ad2859fd1c7e60f0e13570c83
ivandyuzhev <axensk@yandex.ru>
2024-07-20 14:08:10 +0700

    0.3. Тестирование исключений с junit [#219366 #505235]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -14,5 +14,14 @@ class FactorialTest {
                exception.calc(-1);
             });
         }
-
+    @Test
+    void whenCorrectFactorial() {
+        Factorial factorial = new Factorial();
+        assertEquals(1, factorial.calc(0));
+        assertEquals(1, factorial.calc(1));
+        assertEquals(2, factorial.calc(2));
+        assertEquals(6, factorial.calc(3));
+        assertEquals(24, factorial.calc(4));
+        assertEquals(120, factorial.calc(5));
+    }
 }
