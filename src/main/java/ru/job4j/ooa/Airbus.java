0c549cedde20e77ae729c3f3a4e518bb7fb71b5f
ivandyuzhev <axensk@yandex.ru>
2024-07-27 12:38:35 +0700

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,37 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+    private static final int A380_COUNT_ENGINE = 4;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        final int countEngine = "A380".equals(name) ? A380_COUNT_ENGINE : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
