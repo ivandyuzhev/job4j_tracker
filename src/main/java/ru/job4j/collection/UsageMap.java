05eda921472c37b51425c9eddbbc245a5d277aef
ivandyuzhev <axensk@yandex.ru>
2024-09-09 13:46:59 +0700

    0. Map, HashMap [#212652 #512088]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("yourEmail@yandex.ru", "Ivan Dyuzhev");
+        map.put("yourEmail2@yandex.ru", "Sergei Petrov");
+        map.put("yourEmail@yandex.ru", "Ivan Dyuzhev");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
